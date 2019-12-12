fe36c2e17f02f8f963251fbc9414b1caf5893e9c
MonBet <kudampas@gmail.com>
2019-12-12 13:01:07 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model_nb.pkl', 'rb'))
 
 
 @app.route('/')
@@ -13,7 +13,7 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
@@ -26,10 +26,16 @@ def predict():
 def results():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
-
+    #prediction = model.predict_proba([np.array(list(data.values()))])
     output = prediction[0]
     return jsonify(output)
 
+@app.route('/api/', methods=['POST'])
+def makecalc():
+    j_data = request.get_json()
+    #prediction = np.array2string(model.predict(j_data))
+    prediction = np.array2string(model.predict_proba(j_data))
+    return jsonify(prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
